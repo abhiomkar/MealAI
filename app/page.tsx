de154c663c872342eee6566d9144874d77ffb979
Abhinay Omkar <abhiomkar@gmail.com>
2023-06-23 16:27:37 +0530

    Remove Clerk auth from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
 import Link from "next/link";
-import { redirect } from "next/navigation";
-import { currentUser, UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
-  const user = await currentUser();
-  if (user) {
-    redirect("/meal");
-  }
-
   return (
     <main className="flex flex-col items-center">
       <div className="flex w-full items-center justify-between">
         <h1 className="p-4 text-base font-medium tracking-tighter">
           <Link href="/">Meal AI</Link>
         </h1>
-        <div className="p-4 text-sm font-medium">
-          {user ? (
-            <UserButton afterSignOutUrl="/" />
-          ) : (
-            <Link href="/sign-in">Sign in</Link>
-          )}
-        </div>
       </div>
       <div className="z-10 flex w-full max-w-xl flex-col items-center text-sm">
         <h2 className="lg::text-8xl pb-12 pt-12 text-3xl font-extrabold tracking-tighter md:text-5xl">
